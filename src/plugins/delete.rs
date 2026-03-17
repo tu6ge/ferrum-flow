b7a949117842185993b74d2942bef217184597f7
tu6ge <tu6ge@qq.com>
2026-03-17 19:34:23 +0800

    feat: delete support undo system

--- src/plugins/delete.rs
+++ src/plugins/delete.rs
@@ -1,4 +1,8 @@
-use crate::plugin::{FlowEvent, Plugin};
+use crate::{
+    Edge, Node,
+    canvas::Command,
+    plugin::{FlowEvent, Plugin},
+};
 
 pub struct DeletePlugin;
 
@@ -20,15 +24,55 @@ impl Plugin for DeletePlugin {
     ) -> crate::plugin::EventResult {
         if let FlowEvent::Input(crate::plugin::InputEvent::KeyDown(ev)) = event {
             if ev.keystroke.key == "delete" || ev.keystroke.key == "backspace" {
-                if ctx.graph.remove_selected_edge() {
-                    ctx.notify();
-                }
-                if ctx.graph.remove_selected_node() {
-                    ctx.notify();
-                }
+                ctx.execute_command(DeleteCommand::new(&ctx));
                 return crate::plugin::EventResult::Stop;
             }
         }
         crate::plugin::EventResult::Continue
+    }
+}
+
+struct DeleteCommand {
+    selected_edge: Vec<Edge>,
+    selected_node: Vec<Node>,
+}
+
+impl DeleteCommand {
+    fn new(ctx: &crate::plugin::PluginContext) -> Self {
+        Self {
+            selected_edge: ctx
+                .graph
+                .selected_edge
+                .iter()
+                .filter_map(|id| ctx.graph.edges.get(id).cloned())
+                .collect(),
+            selected_node: ctx
+                .graph
+                .selected_node
+                .iter()
+                .filter_map(|id| ctx.get_node(id).cloned())
+                .collect(),
+        }
+    }
+}
+
+impl Command for DeleteCommand {
+    fn name(&self) -> &'static str {
+        "delete"
+    }
+    fn execute(&mut self, ctx: &mut crate::canvas::CanvasState) {
+        ctx.remove_selected_edge();
+        ctx.remove_selected_node();
+    }
+    fn undo(&mut self, ctx: &mut crate::canvas::CanvasState) {
+        for edge in &self.selected_edge {
+            ctx.add_edge(edge.clone());
+            ctx.add_selected_edge(edge.id, true);
+        }
+
+        for node in &self.selected_node {
+            ctx.add_node(node.clone());
+            ctx.add_selected_node(node.id, true);
+        }
     }
 }
