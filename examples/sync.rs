551cf0a6402971484808cc3b2ad7b37a184b9ef1
tu6ge <tu6ge@qq.com>
2026-03-26 17:30:36 +0800

    feat: init YrsSyncPlugin

---
+++ examples/sync.rs
@@ -0,0 +1,53 @@
+use ferrum_flow::*;
+use gpui::{AppContext as _, Application, Size, WindowOptions, px};
+
+fn main() {
+    Application::new().run(|cx| {
+        let mut graph = Graph::new();
+
+        graph
+            .create_node("")
+            .position(100.0, 100.0)
+            .output()
+            .output()
+            .output_with(PortPosition::Bottom, Size::new(px(20.0), px(20.0)))
+            .output_at(PortPosition::Bottom)
+            .build(&mut graph);
+
+        graph
+            .create_node("")
+            .position(300.0, 400.0)
+            .input()
+            .input_at(PortPosition::Top)
+            .input_at(PortPosition::Top)
+            .output()
+            .output_at(PortPosition::Bottom)
+            .output_at(PortPosition::Bottom)
+            .build(&mut graph);
+
+        graph
+            .create_node("")
+            .position(500.0, 500.0)
+            .input()
+            .output()
+            .build(&mut graph);
+
+        cx.open_window(WindowOptions::default(), |_, cx| {
+            cx.new(|ctx| {
+                FlowCanvas::builder(graph, ctx)
+                    .plugin(SelectionPlugin::new())
+                    .plugin(NodeInteractionPlugin::new())
+                    .plugin(ViewportPlugin::new())
+                    .plugin(BackgroundPlugin::new())
+                    .plugin(NodePlugin::new())
+                    .plugin(PortInteractionPlugin::new())
+                    .plugin(EdgePlugin::new())
+                    .plugin(DeletePlugin::new())
+                    .plugin(HistoryPlugin::new())
+                    .sync_plugin(YrsSyncPlugin::new())
+                    .build()
+            })
+        })
+        .unwrap();
+    });
+}
