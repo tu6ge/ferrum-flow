17571a69093e416876675141ae32ceed040da81d
tu6ge <tu6ge@qq.com>
2026-04-25 18:00:18 +0800

    style: rename clipboard inner file

--- crates/core/src/plugins/clipboard/mod.rs
+++ crates/core/src/plugins/clipboard/mod.rs
@@ -1,8 +1,8 @@
-mod clipboard;
 mod clipboard_ops;
 mod copied_subgraph;
+mod plugin;
 
-pub use clipboard::ClipboardPlugin;
+pub use plugin::ClipboardPlugin;
 
 pub(crate) use clipboard_ops::{
     extract_subgraph, get_clipboard_subgraph, has_clipboard_subgraph, paste_subgraph,
