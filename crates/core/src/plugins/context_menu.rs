f43188a9665ce854a1d4cfb3fdd05c11eacc3073
tu6ge <tu6ge@qq.com>
2026-05-04 09:44:35 +0800

    style

--- crates/core/src/plugins/context_menu.rs
+++ crates/core/src/plugins/context_menu.rs
@@ -299,8 +299,6 @@ impl Plugin for ContextMenuPlugin {
         "context_menu"
     }
 
-    fn setup(&mut self, _ctx: &mut crate::plugin::InitPluginContext) {}
-
     fn priority(&self) -> i32 {
         132
     }
